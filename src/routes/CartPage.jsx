66461bdd2c24e8c4fb003e859e3c2c8c7a27946e
kik8727 <kik87827@gmail.com>
2025-07-02 23:51:37 +0900

    진도4

--- src/routes/CartPage.jsx
+++ src/routes/CartPage.jsx
@@ -1,28 +1,36 @@
 import { Table } from "react-bootstrap";
+import { useSelector } from "react-redux";
 
 function CartPage() {
+
+    let state = useSelector((state) => state);
+    let cartData = state.cartData;
+    console.log(cartData);
+
     return (
-        <div>
-            <Table>
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>상품명</th>
-                        <th>수량</th>
-                        <th>변경하기</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>안녕</td>
-                        <td>안녕</td>
-                        <td>안녕</td>
-                    </tr>
-                </tbody>
-            </Table>
-        </div>
-    )
+      <div>
+        <Table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>상품명</th>
+              <th>수량</th>
+              <th>변경하기</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cartData.map((data) => (
+              <tr>
+                <td>{data.id}</td>
+                <td>{data.name}</td>
+                <td>{data.count}</td>
+                <td>변경버튼</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </div>
+    );
 }
 
 export default CartPage;
